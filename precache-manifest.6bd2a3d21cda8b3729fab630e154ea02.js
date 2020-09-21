ec892170341adaa77e6a387bb546ad1a97958ff7
bue221 <camiloplaza3@gmail.com>
2020-09-21 06:00:10 -0500

    Updates

---
+++ precache-manifest.6bd2a3d21cda8b3729fab630e154ea02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ab6aa9b56b8e58c47a4d20eb4f4798a",
+    "url": "/to-do-list-react/index.html"
+  },
+  {
+    "revision": "4c16b5b4cbc15128e970",
+    "url": "/to-do-list-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "83dbb1a30385bd4ca0a8",
+    "url": "/to-do-list-react/static/js/2.de68bf51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-react/static/js/2.de68bf51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c16b5b4cbc15128e970",
+    "url": "/to-do-list-react/static/js/main.1683f860.chunk.js"
+  },
+  {
+    "revision": "4ed71f2ad95994f50178",
+    "url": "/to-do-list-react/static/js/runtime-main.3295b1ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-list-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
